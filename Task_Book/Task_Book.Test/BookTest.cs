bc632254228cce68c7b79ba5e1adf51e03c6bd46
ZheldakArtem <artemzheldak1995@gmail.com>
2016-07-13 18:36:30 +0300

    Adding test method "Book_Sort_By_Criterion"

--- Task_Book/Task_Book.Test/BookTest.cs
+++ Task_Book/Task_Book.Test/BookTest.cs
@@ -11,11 +11,42 @@ namespace Task_Book.Test
 {
     public class BookTest
     {
-        public void Book_Sort_With_Criterion(Book[] arrBooks, Book[] expectedBooks, Criterion criterion)
+        [Test,TestCaseSource(nameof(BooksSortData))]
+        public void Book_Sort_By_Criterion(Book[] arrBooks, Book[] expectedBooks, Criterion criterion)
         {
             SortBooks(arrBooks, criterion);
             CollectionAssert.AreEqual(arrBooks, expectedBooks);
         }
 
-       }
+
+        public static Book[] BooksData { get; } = {
+            new Book("CCC","AAA","BBB",13),
+            new Book("AAA","BBB","CCC",1),
+            new Book("BBB","CCC","AAA",14)
+        };
+
+        public static Book[] ExpectedBooksByAuthor { get; } = {
+            new Book("AAA", "BBB", "CCC", 1),
+            new Book("BBB", "CCC", "AAA", 14),
+            new Book("CCC", "AAA", "BBB", 13)
+        };
+
+        public static Book[] ExpectedBooksByTitle { get; } = {
+            new Book("CCC", "AAA", "BBB", 13),
+            new Book("AAA", "BBB", "CCC", 1),
+            new Book("BBB", "CCC", "AAA", 14)
+        };
+
+        public static Book[] ExpectedBooksByYear { get; } = {
+            new Book("AAA", "BBB", "CCC", 1),
+            new Book("CCC", "AAA", "BBB", 13),
+            new Book("BBB", "CCC", "AAA", 14)
+        };
+
+        public static object[] BooksSortData { get; } = {
+            new object[] {BooksData, Criterion.Author, ExpectedBooksByAuthor},
+            new object[] {BooksData, Criterion.Title, ExpectedBooksByTitle},
+            new object[] {BooksData, Criterion.Year, ExpectedBooksByYear},
+        };
+    }
 }
